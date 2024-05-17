1f6369388021b7b52779a9371e8f462c54e1cb26
IIabdullah97 <zahrani212@hotmail.com>
2024-05-17 18:01:47 +0300

    Update BA.py

--- BA.py
+++ BA.py
@@ -17,24 +17,10 @@ st.markdown(
     }
     </style>
     <div class="custom-text">
-        Hi Bader, it is easy to develop a web but hard to deploy/publish our local host interface
+        Hi Hisham, "الشمس لا تغطى بغربال، الاتحاد هو كبير آسيا"
     </div>
     """,
     unsafe_allow_html=True
 )
-st.markdown(
-    """
-    <style>
-    .custom-text {
-        color: blue;
-        font-size: 24px;
-        font-weight: bold;
-    }
-    </style>
-    <div class="custom-text">
-        Hi Bader, it is easy to develop a web but hard to deploy/publish our local host interface
-    </div>
-    """,
-    unsafe_allow_html=True
-)
+
 
