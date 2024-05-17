5e44ff84ae338f92edc4852b3b1b4869bce25099
IIabdullah97 <zahrani212@hotmail.com>
2024-05-17 17:56:29 +0300

    Update BA.py

--- BA.py
+++ BA.py
@@ -5,5 +5,19 @@ import streamlit as st
 st.title("Welcome")
 
 
-st.write("Hi Bader, it is easy to develop a web but hard to deploy/publish our local host interface")
+st.markdown(
+    """
+    <style>
+    .custom-text {
+        color: blue;
+        font-size: 24px;
+        font-weight: bold;
+    }
+    </style>
+    <div class="custom-text">
+        Hi Bader, it is easy to develop a web but hard to deploy/publish our local host interface
+    </div>
+    """,
+    unsafe_allow_html=True
+)
 
